90bc16d936dc042bff7add95c2675104953282fc
KangDongbum <eaststar0201@naver.com>
2021-09-08 18:40:13 +0900

    자바연습

---
+++ java_source/exam01/src/chapter13_4/Calc.java
@@ -0,0 +1,5 @@
+package chapter13_4;
+
+public interface Calc {
+	public int add (int num1, int num2);
+}
---
+++ java_source/exam01/src/chapter15_1/Ex2.java
@@ -0,0 +1,22 @@
+package chapter15_1;
+
+import java.io.IOException;
+
+public class Ex2 {
+	public static void main(String[] args) throws IOException {
+		try {
+			System.out.println("문자 1개를 입력하세요: ");
+			int i = System.in.read(); 
+			/**
+			*	byte -> unsigned byte (0을 포함한 양수)
+			* 	-128~127
+			* 	unsigned 0~255
+			* 	전부 다 읽은 경우 -1을 반환
+			* 	int -> char
+			*/
+			System.out.println((char)i);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
